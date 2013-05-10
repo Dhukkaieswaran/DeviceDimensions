8d4a1baad8562a73f6759a8c22533c89c2896062
Dhukkaieswaran <eswaran1@gmail.com>
2013-05-10 16:00:47 +0530

    Version 1.0.0
    Test Version

---
+++ src/Device/Dimensions/DeviceDimensionsActivity.java
@@ -0,0 +1,64 @@
+package Device.Dimensions;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.view.Display;
+import android.view.View;
+import android.widget.TextView;
+
+public class DeviceDimensionsActivity extends Activity {
+    /** Called when the activity is first created. */
+	Display ApplicationDisplay;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ApplicationDisplay = getWindowManager().getDefaultDisplay();
+        String deviceId = Secure.getString(this.getContentResolver(),Secure.ANDROID_ID);
+        
+        TextView WidthTextview=(TextView)findViewById(R.id.WidthtextView1);
+        TextView HeighttextView=(TextView)findViewById(R.id.HeighttextView2);
+        TextView IDtextView=(TextView)findViewById(R.id.IDtextView2);
+        
+        WidthTextview.setText("Width : "+String.valueOf(ApplicationDisplay.getWidth()));
+        HeighttextView.setText("Height : "+String.valueOf(ApplicationDisplay.getHeight()));
+        IDtextView.setText("ANDROID ID : "+deviceId);
+        
+// switch1
+//        final Switch switchT1 = (Switch)findViewById(R.id.switch1);   
+//        switchT1.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+//                if(switchT1.isChecked() == true)
+//                {
+////                	txtView1.setText("Your Selected is On");
+//        			Toast.makeText(DeviceDimensionsActivity.this,
+//            				String.valueOf("Your Selected is On"),
+//            					Toast.LENGTH_SHORT).show();          	
+//                }
+//                else
+//                {
+////                	txtView1.setText("Your Selected is Off");
+//        			Toast.makeText(DeviceDimensionsActivity.this,
+//            				String.valueOf("Your Selected is Off"),
+//            					Toast.LENGTH_SHORT).show();          	
+//                }            	
+//   
+//            }
+//        });
+
+    }
+    
+    public void ExitApplication(View v)
+    {
+    	try
+    	{
+    		finish();
+    	}catch (Exception e) 
+    	{
+			// TODO: handle exception
+		}
+    }
+}
